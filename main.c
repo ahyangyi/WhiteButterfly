940ffce57907d32ba95f58c1723909eb60f74bd1
Yi Yang @ Anteros <ahyangyi@gmail.com>
2021-07-04 20:09:24 +0800

    Add extern to eclass.

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ struct enemystruct enemy [NO_ENEMIES];
 struct ebulletstruct ebullet [NO_EBULLETS];
 struct pbulletstruct pbullet [NO_PBULLETS];
 struct arenastruct arena;
-struct eclassstruct eclass [NO_ENEMY_TYPES];
+extern struct eclassstruct eclass [NO_ENEMY_TYPES];
 struct cloudstruct cloud [NO_CLOUDS];
 struct bossstruct boss;
 
